335595c1694d1ac06f8e5e071ebe5b1f74f8fb3c
oreldm <orel@mip.me>
2018-04-06 09:09:59 +0300

    intent to activity

---
+++ MemoryGame/app/src/main/java/com/example/h1z1/memorygame/Activities/StartGameActivity.java
@@ -0,0 +1,32 @@
+package com.example.h1z1.memorygame.Activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.h1z1.memorygame.R;
+
+public class StartGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_game);
+
+        Bundle welcomePageData = getIntent().getExtras();
+
+        if(welcomePageData==null){
+            return;
+        }
+        String username = welcomePageData.getString("username");
+        String age = welcomePageData.getString("age");
+        ((TextView)findViewById(R.id.usernameID)).setText(username);
+        ((TextView)findViewById(R.id.textView5)).setText(age);
+    }
+
+}
---
+++ MemoryGame/app/src/main/java/com/example/h1z1/memorygame/Activities/WelcomePageActivity.java
@@ -0,0 +1,30 @@
+package com.example.h1z1.memorygame.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import com.example.h1z1.memorygame.R;
+
+public class WelcomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_page);
+    }
+
+    public void onClick(View view){
+        Intent i = new Intent(this, StartGameActivity.class);
+        final EditText usernameEdit= (EditText) findViewById(R.id.nameInputID);
+        final EditText ageEdit = (EditText) findViewById(R.id.ageInputID);
+        String username=usernameEdit.getText().toString();
+        String age = ageEdit.getText().toString();
+        i.putExtra("username",username);
+        i.putExtra("age",age);
+
+        startActivity(i);
+    }
+
+}
