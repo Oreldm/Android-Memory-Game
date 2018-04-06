5b9636104832945175e099f5586af683e28803c1
oreldm <orel@mip.me>
2018-04-06 06:29:51 +0300

    first commit

---
+++ MemoryGame/app/src/main/java/com/example/h1z1/memorygame/WelcomePage.java
@@ -0,0 +1,14 @@
+package com.example.h1z1.memorygame;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class WelcomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_page);
+    }
+
+}
